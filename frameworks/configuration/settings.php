96f4c561de9a074bc50a5568431159a022f31e6a
pburney <pburney>
2008-06-17 15:22:17 +0000

    PB: Basic RI-ITEST Portal

--- frameworks/configuration/settings.php
+++ frameworks/configuration/settings.php
@@ -59,15 +59,15 @@ $portal_config['project_settings']['udl']['accommodations_name'] = 'UDL Settings
 $portal_config['project_settings']['udl']['show_activities_link_to_students'] = 'no';
 $portal_config['project_settings']['udl']['activities_navigation_word'] = 'Units';
 
-$portal_config['project_settings']['udl']['diy_server'] = 'rails.dev.concord.org';
-$portal_config['project_settings']['udl']['diy_server_path'] = '/udl';
+$portal_config['project_settings']['udl']['diy_server'] = 'udldiy.concord.org';
+$portal_config['project_settings']['udl']['diy_server_path'] = '';
 $portal_config['project_settings']['udl']['diy_database'] = 'rails';
-$portal_config['project_settings']['udl']['diy_table_prefix'] = 'udl_';
+$portal_config['project_settings']['udl']['diy_table_prefix'] = 'udldiy_';
 $portal_config['project_settings']['udl']['diy_activities_name'] = 'external_otrunk_activities';
 $portal_config['project_settings']['udl']['diy_runnable_type_name'] = 'ExternalOtrunkActivity';
 $portal_config['project_settings']['udl']['diy_manager_user'] = 'ccadmin';
 $portal_config['project_settings']['udl']['diy_manager_password'] = 'h0ru$';
-$portal_config['project_settings']['udl']['diy_session_name'] = '_Universal_Design_in_Science_Education_OWCgmRUr8s1GQfrvi2SItw____session_id';
+$portal_config['project_settings']['udl']['diy_session_name'] = '_Universal_Design_in_Science_Education__MnlhAlM0WQGLne_J_9Nig____session_id';
 $portal_config['project_settings']['udl']['diy_use_uuid'] = 'yes';
 $portal_config['project_settings']['udl']['diy_param_order'] = 'interface/member';
 $portal_config['project_settings']['udl']['diy_reporting_parameter'] = '?reporting=reporting';
@@ -78,6 +78,7 @@ $portal_config['project_settings']['udl']['extra_navigation_items'][] = array(
 	'value' => 'http://udl.concord.org/share/teacher-guides/',
 	'deny' => 'student'
 );
+
 
 
 // CAPA Project
@@ -103,6 +104,30 @@ $portal_config['project_settings']['capa']['diy_param_order'] = 'interface/membe
 $portal_config['project_settings']['capa']['diy_reporting_parameter'] = '?reporting=report_mode';
 
 $portal_config['project_settings']['capa']['extra_navigation_items'] = array();
+
+// CAPA Project
+
+$portal_config['project_settings']['ri-itest']['site_template'] = 'website/ri-itest-template.php';
+$portal_config['project_settings']['ri-itest']['available_actions'] = array('info', 'report', 'run', 'try');
+$portal_config['project_settings']['ri-itest']['use_diy_activities'] = 'no';
+$portal_config['project_settings']['ri-itest']['show_activities_link_to_students'] = 'no';
+$portal_config['project_settings']['ri-itest']['show_front_page_image'] = 'yes';
+$portal_config['project_settings']['ri-itest']['activities_navigation_word'] = 'Activities';
+
+$portal_config['project_settings']['ri-itest']['diy_server'] = 'ri-itest.diy.concord.org';
+$portal_config['project_settings']['ri-itest']['diy_server_path'] = '';
+$portal_config['project_settings']['ri-itest']['diy_database'] = 'riitestdiy';
+$portal_config['project_settings']['ri-itest']['diy_table_prefix'] = 'riitestdiy_';
+$portal_config['project_settings']['ri-itest']['diy_activities_name'] = 'external_otrunk_activities';
+$portal_config['project_settings']['ri-itest']['diy_runnable_type_name'] = 'ExternalOtrunkActivity';
+$portal_config['project_settings']['ri-itest']['diy_manager_user'] = 'cstaudt';
+$portal_config['project_settings']['ri-itest']['diy_manager_password'] = 'cstaudt';
+$portal_config['project_settings']['ri-itest']['diy_session_name'] = '_ri-itest_6u52dphuaWIvhMslWT2p0A____session_id';
+$portal_config['project_settings']['ri-itest']['diy_use_uuid'] = 'no';
+$portal_config['project_settings']['ri-itest']['diy_param_order'] = 'interface/member';
+$portal_config['project_settings']['ri-itest']['diy_reporting_parameter'] = '?reporting=report_mode';
+
+$portal_config['project_settings']['ri-itest']['extra_navigation_items'] = array();
 
 
 // PAEMST Project
