ea2c0597f8dc59ab6d654d8da4347c1ecbd3415a
aunger <aunger>
2008-12-30 16:53:49 +0000

    Added timing information to the statistics page.

--- frameworks/controllers/usage-statistics.php
+++ frameworks/controllers/usage-statistics.php
@@ -1,15 +1,17 @@
 <?php
+   $script_start = microtime(true);
+
    $page_title = "Statistics";
    echo '<div class="statistics">';
-   /* if the current user isn't an admin, redirect to the home page */
+   // if the current user isn't an admin, redirect to the home page
    echo '<ul>';
    $portal_teachers = portal_get_all_teachers($_PORTAL['project_info']['project_id']);
    // print_r($portal_teachers);
-   /* for each teacher in the current portal */
+   // for each teacher in the current portal
    foreach ($portal_teachers as $teacher) {
      echo '<li>' . $teacher['member_first_name'] . " " . $teacher['member_last_name'] . " (" . $teacher['member_id'] . ")" . "</li>";
      $class_ids = portal_get_teacher_classes($teacher['member_id']);
-     /* for each class that the teacher has */
+     // for each class that the teacher has
      echo '<ul>';
      foreach ($class_ids as $class_id) {
        $class_activities = portal_get_class_diy_activities($class_id);
@@ -21,9 +23,9 @@
          continue;
        }
        $class = portal_get_class_info($class_id);
-       /* count the number of students registered */
+       // count the number of students registered
        echo '<li>' . $class['class_name'] . ': ' . count($registered_students) . ' students</li>';
-       /* count the number of students that have a learner session */
+       // count the number of students that have a learner session
        $activity_usage = array();
        foreach ($registered_students as $stu) {
          $used = portal_get_diy_activity_usage_from_db($stu['member_id']);
@@ -35,7 +37,7 @@
            }
          }
        }
-       /* list the activities for the class */
+       // list the activities for the class
        echo '<ul>';
        foreach ($class_activities as $activity) {
          echo '<li>' . $activity['activity_name'] . ' (' . (array_key_exists($activity['activity_id'], $activity_usage) ? $activity_usage[$activity['activity_id']] : "0") . ' active students)' . '</li>';
@@ -43,7 +45,14 @@
        echo '</ul>';
      }
      echo '</ul>';
+
    }
    echo '</ul>';
    echo '</div>';
+
+   $script_end = microtime(true);
+
+   $elapsed_time = round($script_end - $script_start, 5);
+
+   echo '<div style="">Rendered in ' . $elapsed_time . ' seconds</div>';
 ?>
